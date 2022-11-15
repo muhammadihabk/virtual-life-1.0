4dc4e609ea5636e18ed0df904c62fc2fb614b1fa
muhammad <69121442+muhammadihabk@users.noreply.github.com>
2022-11-15 16:13:31 +0200

    Cleaned

--- src/main/java/com/example/virtual_life/controller/UserController.java
+++ src/main/java/com/example/virtual_life/controller/UserController.java
@@ -3,7 +3,6 @@ package com.example.virtual_life.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,12 +24,6 @@ public class UserController {
     public UserController(UserService userService) {
         this.userService = userService;
     }
-
-    // // public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
-    // @Override
-    // public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
-
-    // }
 
     @PostMapping
     public User registerUser(@RequestBody User user) {
